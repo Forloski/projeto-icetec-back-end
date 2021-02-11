687a18d012aa81f2a1bf5dd7d090a12acd636a20
Forloski <f.r.orloski@gmail.com>
2021-02-11 13:55:44 -0300

    Deleted unused documents

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,6 @@
-// src/routes/index.ts
 import 'reflect-metadata';
 import { Router } from 'express';
-// import providersRouter from '@modules/candidates/infra/http/routes/providers.routes';
+
 import candidatesRouter from '@modules/candidates/infra/http/routes/candidates.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
@@ -11,6 +10,5 @@ const routes = Router();
 routes.use('/candidates', candidatesRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-// routes.use('/providers', providersRouter);
 
 export default routes;
